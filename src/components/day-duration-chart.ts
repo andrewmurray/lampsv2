1574772a29fe3b9e662db21d1a0949024a5f50b9
Andrew Murray <sckoobs@gmail.com>
2016-12-15 07:03:56 +0000

    Starting on the day duratiion d2 chart

---
+++ src/components/day-duration-chart.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: "day-duration-chart",
+  templateUrl: ''
+})
+export default class DayDurationChart {
+  constructor() {
+  }
+}
