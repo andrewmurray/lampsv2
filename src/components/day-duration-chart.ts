45d49f0d8e90994218cafd366ef845f89de9d46c
Andrew Murray <sckoobs@gmail.com>
2016-12-15 16:56:06 +0000

    Work on the day duration chart, need to fix an unknown type error

--- src/components/day-duration-chart.ts
+++ src/components/day-duration-chart.ts
@@ -1,11 +1,26 @@
-import {Component} from 'angular2/core';
+import {Component, ViewContainerRef} from '@angular2/core';
 import * as d3 from 'd3';
 
 @Component({
   selector: "day-duration-chart",
-  templateUrl: ''
+  template: '<ng-content/>'
 })
 export default class DayDurationChart {
-  constructor() {
+  private host;
+  private width: number;
+  private height: number;
+  private htmlElement: HTMLElement;
+
+  constructor(private viewContainerRef: ViewContainerRef) {
+    this.htmlElement = viewContainerRef.element.nativeElement;
+    this.host = d3.select(viewContainerRef.nativeElement);
+  }
+
+  private setUp() : void {
+    this.width = this.htmlElement.clientWidth;
+  }
+
+  ngOnChange() : void {
+    this.setUp();
   }
 }
