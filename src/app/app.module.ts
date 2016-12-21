13df8faa063390c170553e2e56c0147a5e47d208
Andrew Murray <sckoobs@gmail.com>
2016-12-21 22:10:51 +0000

    Fixed merge mess up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Plans } from '../pages/plans/plans';
-<<<<<<< HEAD
 import { EditPlan } from '../pages/plans/edit';
-=======
 import { DayDurationChart } from '../components/day-duration-chart';
->>>>>>> origin/master
 
 @NgModule({
   declarations: [
     MyApp,
     Plans,
-<<<<<<< HEAD
-    EditPlan
-=======
+    EditPlan,
     DayDurationChart
->>>>>>> origin/master
   ],
   imports: [
     IonicModule.forRoot(MyApp)
